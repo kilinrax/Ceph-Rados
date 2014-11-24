5433d7a1bfe9b94172bfeede639e5381ed73d95a
Alex Bowley <alex@openimp.com>
2014-11-24 16:05:49 +0000

    Stub documentation.

--- lib/Ceph/Rados.pm
+++ lib/Ceph/Rados.pm
@@ -22,14 +22,8 @@ sub AUTOLOAD {
     my ($error, $val) = constant($constname);
     if ($error) { croak $error; }
     {
-	no strict 'refs';
-	# Fixed between 5.005_53 and 5.005_61
-#XXX	if ($] >= 5.00561) {
-#XXX	    *$AUTOLOAD = sub () { $val };
-#XXX	}
-#XXX	else {
+        no strict 'refs';
 	    *$AUTOLOAD = sub { $val };
-#XXX	}
     }
     goto &$AUTOLOAD;
 }
@@ -74,44 +68,46 @@ __END__
 
 =head1 NAME
 
-Ceph::Rados - Perl extension for blah blah blah
+Ceph::Rados - Perl wrapper to librados.
 
 =head1 SYNOPSIS
 
   use Ceph::Rados;
-  blah blah blah
+
+  my $cluster = Ceph::Rados->new('admin');
+  $cluster->set_config_file;
+  $cluster->set_config_option( keyring => '/etc/ceph/ceph.client.admin.keyring');
+  $cluster->connect;
+
+  my $io = $cluster->io('testing_pool');
+  $io->write('greeting', 'hello');
+  my $stored_data = $io->read('greeting');
+  $io->delete('greeting');
+
+  my $list = $io->list;
+  while (my $entry = $list->next) {
+      print "Found $entry\n";
+  }
 
 =head1 DESCRIPTION
 
-Stub documentation for Ceph::Rados, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
+This module provides a very limited subset of the librados API,
+currently just read/writes and lists.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+librados documentation - L<http://ceph.com/docs/master/rados/api/librados/>
 
 =head1 AUTHOR
 
-Alex, E<lt>alex@E<gt>
+Alex Bowley, E<lt>alex@openimp.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014 by Alex
+Copyright (C) 2014 by Alex Bowley.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
