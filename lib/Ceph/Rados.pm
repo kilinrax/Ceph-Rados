170de72dd2721e09141be7056dfca61c7e68c0e7
Alex Bowley <alex@openimp.com>
2017-08-25 17:27:02 +0100

    Version bump

--- lib/Ceph/Rados.pm
+++ lib/Ceph/Rados.pm
@@ -8,7 +8,7 @@ use AutoLoader;
 use Carp;
 use Ceph::Rados::IO;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 # Do we have C symbols in  a Ceph::Rados::Constants::C::Symbols module?
 my $_symbols_present;
